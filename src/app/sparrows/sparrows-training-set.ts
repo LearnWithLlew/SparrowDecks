be4c0704c75c0f411e5c99ee00dea1c83ecc2ea5
Llewellyn Falco <isidore@setgame.com>
2016-09-12 04:13:40 -0500

    Handles house & song

--- src/app/sparrows/sparrows-training-set.ts
+++ src/app/sparrows/sparrows-training-set.ts
@@ -1,18 +1,41 @@
+function create(item1, item2){
+  let results = [];
+  let list1 = getItems(item1);
+  let list2 = getItems(item2);
+  while(0 < list1.length  && 0 < list2.length){
+    var first = Math.random() >= 0.5;
+    var from = first ? list1 : list2;
+    if (0 < from.length){
+      results.push(from.shift())
+    }
+  }
+  return results;
+}
+function pad(number, length)
+{
+  var s = number+"";
+  while (s.length < length) {
+    s = "0" + s;
+  }
+  return s;
+}
+
+
+function getItems(item1){
+  let results = []
+  for (var i = 1; i <= item1.number; i++) {
+    results.push( {
+    "image": item1.name + pad(i,2)+ item1.prefix,
+    "answer": item1.name
+    });
+  }
+  return results;
+}
 
 export const sparrowsTrainingSet = {
   title: "Sparrows",
-  left: "HOUSE",
-  right: "SONG",
+  left: "House",
+  right: "Song",
   baseUrl: "sparrow/images/",
-  examples: [
-    {
-      "image": "image1.jpeg",
-      "answer": "SONG"
-    },
-    {
-      "image": "image2.jpeg",
-      "answer": "HOUSE"
-    }
-  ]
+  examples: create({ name:"Song", number:50, prefix:".jpeg"}, { name:"House", number:50, prefix:".jpeg"})
 };
-
