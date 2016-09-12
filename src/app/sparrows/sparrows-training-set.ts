e7d60c49e227edaf4ac3bfaaa854887c0ce8c943
Llewellyn Falco <isidore@setgame.com>
2016-09-12 05:36:48 -0500

    starting css

--- src/app/sparrows/sparrows-training-set.ts
+++ src/app/sparrows/sparrows-training-set.ts
@@ -1,41 +1,9 @@
-function create(item1, item2){
-  let results = [];
-  let list1 = getItems(item1);
-  let list2 = getItems(item2);
-  while(0 < list1.length  && 0 < list2.length){
-    var first = Math.random() >= 0.5;
-    var from = first ? list1 : list2;
-    if (0 < from.length){
-      results.push(from.shift())
-    }
-  }
-  return results;
-}
-function pad(number, length)
-{
-  var s = number+"";
-  while (s.length < length) {
-    s = "0" + s;
-  }
-  return s;
-}
-
-
-function getItems(item1){
-  let results = []
-  for (var i = 1; i <= item1.number; i++) {
-    results.push( {
-    "image": item1.name + pad(i,2)+ item1.prefix,
-    "answer": item1.name
-    });
-  }
-  return results;
-}
+import { createTrainingSet } from './training-set';
 
 export const sparrowsTrainingSet = {
   title: "Sparrows",
   left: "House",
   right: "Song",
   baseUrl: "sparrow/images/",
-  examples: create({ name:"Song", number:50, prefix:".jpeg"}, { name:"House", number:50, prefix:".jpeg"})
+  examples: createTrainingSet({ name:"Song", number:50, prefix:".jpeg"}, { name:"House", number:50, prefix:".jpeg"})
 };
---
+++ src/app/sparrows/training-set.ts
@@ -0,0 +1,32 @@
+export function createTrainingSet(item1, item2){
+  let results = [];
+  let list1 = getItems(item1);
+  let list2 = getItems(item2);
+  while(0 < list1.length  && 0 < list2.length){
+    var first = Math.random() >= 0.5;
+    var from = first ? list1 : list2;
+    if (0 < from.length){
+      results.push(from.shift())
+    }
+  }
+  return results;
+}
+function pad(number, length){
+  var s = number+"";
+  while (s.length < length) {
+    s = "0" + s;
+  }
+  return s;
+}
+
+
+function getItems(item1){
+  let results = []
+  for (var i = 1; i <= item1.number; i++) {
+    results.push( {
+    "image": item1.name + pad(i,2)+ item1.prefix,
+    "answer": item1.name
+    });
+  }
+  return results;
+}
